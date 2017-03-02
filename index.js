09610d8baf650743eafe22d4706b3ea47b6f401a
Ekaterina Petushova <epetushova@gmail.com>
2017-03-01 20:38:49 -0500

    Done.

--- index.js
+++ index.js
@@ -1,12 +1,1 @@
-// Rectangle constructor
-function Rectangle(sides, width, height) {
-  this.sides = sides;
-  this.width = width;
-  this.height = height;
-  this.area = function() {
-    return this.width * this.height;
-  }
-  this.perimeter = function() {
-    return (this.width + this.height) * 2;
-  }
-}
+const app = "I don't do much."
